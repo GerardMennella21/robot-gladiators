34c6839e5e7b4efa23c7959d50ba19aac9b3a199
Gerard Mennella <gerardmennella21@gmail.com>
2022-02-26 19:07:02 -0500

    Created for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,15 +8,26 @@ var playerMoney = 10;
 console.log(playerName + ",", "Attack:", playerAttack, "Health:", playerHealth, "Money:", playerMoney);
 
 // Enemy Robot
-var enemyName = "Roborto"
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // Enemy Console Log
-console.log(enemyName + ",", "Attack:", enemyAttack, "Health:", enemyHealth);
+console.log(",", "Attack:", enemyAttack, "Health:", enemyHealth);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index")
+}
+
+// Game States
+// "Win" Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "Lose" Player robot's health is zero or less
 
 // Fight Function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // Fight Prompt
@@ -34,10 +45,10 @@ var fight = function() {
         
         // Check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert( + " has died!");
         }
         else {
-            window.alert(enemyName + " still has " +enemyHealth + " health left.");
+            window.alert( enemyName + " still has " +enemyHealth + " health left.");
         }
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
@@ -68,5 +79,6 @@ var fight = function() {
         window.alert("You need to choose a valid option. Try again!");
     }
 };
-
-fight();
+for (var i = 0; i <enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
